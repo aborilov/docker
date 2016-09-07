50fb5aa637319a70b410d571a4d07b70b9439abd
vinfry <epoltaranin@cloudlinux.com>
2016-09-07 16:58:28 +0300

    simple unzip

--- images/1.6.1.6/config_files/ps-extractor.sh
+++ images/1.6.1.6/config_files/ps-extractor.sh
@@ -7,7 +7,7 @@ if [[ -n "$folder" ]]; then
     if [ -d $folder/prestashop ]; then
        mv -v $folder/prestashop/* $destination
     else
-        unzip $folder/prestashop.zip && cp -rp $folder/prestashop/* $destination
+        unzip $folder/prestashop.zip
     fi
 else
     echo "Missing folder to move"
